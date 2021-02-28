5c45626b05ea90382d9b1cc9c982cef119026093
Trang <10674637@uvu.edu>
2021-02-28 11:53:31 -0700

    update the app with amplify cognito

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,45 @@
 <template>
   <div id="app">
-    <amplify-authenticator>
-      <amplify-sign-in header-text="My Custom Sign In Text" slot="sign-in"></amplify-sign-in>
-      <div>
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-        <div id="amplify-signout">
-          <amplify-sign-out></amplify-sign-out>
-        </div>
-      </div>
-    </amplify-authenticator>
+    <div id="nav">
+      <!-- <router-link to="/">Home</router-link> | -->
+      <!-- <router-link to="/about">About</router-link> -->
+      <Navbar />
+    </div>
+    <router-view />
   </div>
 </template>
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
+import { mapActions } from "vuex";
 export default {
-  name: "App",
+  mounted() {
+    this.authAction();
+  },
   components: {
-    HelloWorld
-  }
+    Navbar,
+  },
+  methods: {
+    ...mapActions("auth", ["authAction"]),
+  },
 };
 </script>
-<style>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-#amplify-signout {
-  width: 100px;
-  margin: 0 auto;
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/tailwind.css'
 import '@aws-amplify/ui-vue';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
@@ -9,5 +12,7 @@ Amplify.configure(awsconfig);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
